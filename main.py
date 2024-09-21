0488f24ca99a6e0b27cbd08bfdf4c214b20399a3
Yami <santox25@gmail.com>
2024-09-21 12:23:16 +0200

    Code Archive creation

--- main.py
+++ main.py
@@ -62,21 +62,6 @@ df.drop_duplicates('ID', keep='last')
 #print(f"Unique gender values: {df['CODE_GENDER'].unique()}")
 #changing type from string Y/N to True/False
 
-print(df['FLAG_OWN_CAR'].unique())
-df['FLAG_OWN_CAR'] = df['FLAG_OWN_CAR'].replace({'Y': True, 'N': False}).astype(bool)
-
-print(df['FLAG_OWN_REALTY'].unique())
-df['FLAG_OWN_REALTY'] = df['FLAG_OWN_REALTY'].replace({'Y': True, 'N': False}).astype(bool)
-
-print(df['FLAG_WORK_PHONE'].unique())
-df['FLAG_WORK_PHONE'] = df['FLAG_WORK_PHONE'].replace({'Y': True, 'N': False}).astype(bool)
-
-print(df['FLAG_PHONE'].unique())
-df['FLAG_PHONE'] = df['FLAG_PHONE'].replace({'Y': True, 'N': False}).astype(bool)
-
-print(df['FLAG_EMAIL'].unique())
-df['FLAG_EMAIL'] = df['FLAG_EMAIL'].replace({'Y': True, 'N': False}).astype(bool)
-
 ######## encoding
 
 df_encoded = df.copy()
@@ -106,29 +91,6 @@ for col in categorical_cols:
 
 df = df_encoded
 
-#print(df.head())
-#le_gender = LabelEncoder()
-
-#df['CODE_GENDER'] = le_gender.fit_transform(df['CODE_GENDER'])
-#print(f"Unique occupations: {df['OCCUPATION_TYPE'].unique()}")
-#le_occupations = LabelEncoder()
-#df['OCCUPATION_TYPE'] = le_occupations.fit_transform(df['OCCUPATION_TYPE'])
-
-#le_income = LabelEncoder()
-#print(df['NAME_INCOME_TYPE'].unique())
-#df['NAME_INCOME_TYPE'] = le_income.fit_transform(df['NAME_INCOME_TYPE'])
-
-#print(df['NAME_EDUCATION_TYPE'].unique())
-#le_education = LabelEncoder()
-#df['NAME_EDUCATION_TYPE'] = le_education.fit_transform(df['NAME_EDUCATION_TYPE'])
-
-#print(df['NAME_FAMILY_STATUS'].unique())
-#le_familystatus = LabelEncoder()
-#df['NAME_FAMILY_STATUS'] = le_familystatus.fit_transform(df['NAME_FAMILY_STATUS'])
-
-#print(df['NAME_HOUSING_TYPE'].unique())
-#le_housing = LabelEncoder()
-#df['NAME_HOUSING_TYPE'] = le_familystatus.fit_transform(df['NAME_HOUSING_TYPE'])
 
 
 
